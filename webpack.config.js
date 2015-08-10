8a0e85b66e3e9665d5ef57c82739ff1661bfeeae
Chiara-yen <chiyen.chiara@gmail.com>
2015-08-10 12:09:44 +0800

    add scripts & clean comment

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,9 @@
-var ExtractTextPlugin = require("extract-text-webpack-plugin");
-
 module.exports = {
     entry: "./public/draw-scatterplot-chart.js",
     output: {
         path: __dirname + '/public',
         filename: "bundle.js"
     },
-    // module: {
-    //     loaders: [
-    //         { test: /\.styl$/, loader: ExtractTextPlugin.extract("style-loader", "css-loader", "less-loader") }
-    //     ]
-    // },
-    // plugins: [
-    //     new ExtractTextPlugin("styles.css")
-    // ]
     module: {
         loaders: [
             { test: /\.(jpg|png)$/, loader: 'url-loader?limit=10' },
